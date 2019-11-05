e528abc989b7c93da8a9cbbbba2447a7cf2064f3
Kelvin Wang <sherkrainwang@gmail.com>
2019-11-05 18:56:48 -0500

    feat(src): add flag to specify the logfile location

--- src/loggers.rs
+++ src/loggers.rs
@@ -1,33 +1,21 @@
 use std::fs::{File, OpenOptions};
-use std::io::{self, Write};
+use std::io::Write;
 
 pub trait Logger {
     fn info(&mut self, s: String) -> Result<(), String>;
     fn error(&mut self, s: String) -> Result<(), String>;
 }
 
+#[derive(Default)]
 pub struct DefaultLogger {}
 
-impl DefaultLogger {
-    fn log(&mut self, s: String) -> Result<(), String> {
-        match io::stdout().write_all(s.as_bytes()) {
-            Ok(_) => return Ok(()),
-            Err(_) => return Err("Failed to write log".to_string()),
-        }
-    }
-
-    fn logln(&mut self, s: String) -> Result<(), String> {
-        return self.log(format!("{}\n", s));
-    }
-}
-
 impl Logger for DefaultLogger {
-    fn info(&mut self, s: String) -> Result<(), String> {
-        return self.logln(format!("INFO: {}", s));
+    fn info(&mut self, _: String) -> Result<(), String> {
+        Ok(())
     }
 
-    fn error(&mut self, s: String) -> Result<(), String> {
-        return self.logln(format!("ERROR: {}", s));
+    fn error(&mut self, _: String) -> Result<(), String> {
+        Ok(())
     }
 }
 
@@ -46,13 +34,15 @@ impl Logger for FileLogger {
 }
 
 impl FileLogger {
-    pub fn new(path: &'static str) -> Result<FileLogger, String> {
+    pub fn new<'a>(path: &'a str) -> Result<FileLogger, String> {
         let mut options = OpenOptions::new();
         options.append(true).create(true);
 
         let file = match options.open(path) {
             Ok(f) => f,
-            Err(_) => return Err("Failed to create file logger".to_string()),
+            Err(_) => {
+                return Err("Failed to create file logger".to_string())
+            }
         };
 
         return Ok(FileLogger { file: file });
