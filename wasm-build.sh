f9b7799c71975c1cc13b3543f4e9e283df302c21
Brandon Farmer <bthesorceror@gmail.com>
2020-05-04 16:52:25 -0700

    Fix build script and bump version

--- wasm-build.sh
+++ wasm-build.sh
@@ -8,5 +8,6 @@ wasm-pack build -t browser -d pkg-browser --out-name flux-lsp-browser --scope in
 cat pkg-node/package.json | sed s/\\/flux-lsp\"/\\/flux-lsp-node\"/g > pkg-node/package-new.json
 mv pkg-node/package-new.json pkg-node/package.json
 
-cat pkg-browser/package.json | sed s/\\/flux-lsp\"/\\/flux-lsp-browser\"/g > pkg-browser/package-new.json
+cat pkg-browser/package.json | sed s/\\/flux-lsp\"/\\/flux-lsp-browser\"/g | sed -e 's/"files": \[/"files": [\
+    "flux-lsp-browser_bg.js",/g' > pkg-browser/package-new.json
 mv pkg-browser/package-new.json pkg-browser/package.json
