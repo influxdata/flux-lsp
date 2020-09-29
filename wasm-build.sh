34ae73ceb106d9f6226afd48420f2c03b03997d2
Sean Brickley <sean@wabr.io>
2020-09-29 11:22:24 -0400

    fix(lsp): change wasm-build.sh so it doesn't change the github url in package.json

--- wasm-build.sh
+++ wasm-build.sh
@@ -8,9 +8,9 @@ fi
 wasm-pack build -t nodejs -d pkg-node --out-name flux-lsp-node --scope influxdata --release
 wasm-pack build -t browser -d pkg-browser --out-name flux-lsp-browser --scope influxdata --release
 
-cat pkg-node/package.json | sed s/\\/flux-lsp\"/\\/flux-lsp-node\"/g > pkg-node/package-new.json
+cat pkg-node/package.json | sed s/@influxdata\\/flux-lsp\"/@influxdata\\/flux-lsp-node\"/g > pkg-node/package-new.json
 mv pkg-node/package-new.json pkg-node/package.json
 
-cat pkg-browser/package.json | sed s/\\/flux-lsp\"/\\/flux-lsp-browser\"/g | sed -e 's/"files": \[/"files": [\
+cat pkg-browser/package.json | sed s/@influxdata\\/flux-lsp\"/@influxdata\\/flux-lsp-browser\"/g | sed -e 's/"files": \[/"files": [\
     "flux-lsp-browser_bg.js",/g' > pkg-browser/package-new.json
 mv pkg-browser/package-new.json pkg-browser/package.json
