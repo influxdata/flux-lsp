93e2ca46df9fdd3afb1135d5ab4cd2fe49de3103
Sean Brickley <sean@wabr.io>
2020-10-12 09:43:42 -0400

    fix: simplify release script

--- release.sh
+++ release.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
+which hub &> /dev/null
+if [[ @? != 0 ]]; then
+	echo "Please install the hub command line tool before running this script."
+	echo "https://github.com/github/hub"
+fi
+
 current_branch=$(git branch --show-current)
+if [[ $current_branch != "master" ]]; then
+	echo "You are not on the master branch. Aborting."
+	exit 1
+fi
+
+
 git_changes=$(git status -s | wc -l)
 if [[ $git_changes != 0 ]]; then
-	if [[ $current_branch == "master" ]]; then
-		echo "You have modified the master branch."
-		echo "Please revert or move your changes to another branch before running this script."
-	else
-		echo "Please commit or stash your changes before running this script"
-	fi
+	echo "You have modified the master branch."
+	echo "Please revert or move your changes to another branch before running this script."
 	exit 1
 fi
 
 git fetch
-git checkout master
 ahead=$(git status -sb | grep ahead -c)
 if [[ $ahead != 0 ]]; then
 	echo "Your local master branch is ahead of the remote master branch. Exiting."
 	exit 1
 fi
 
-
 release_type=$1
 if [[ $release_type != "minor" && $release_type != "patch" && $release_type != "major" ]]; then
 	echo "Invalid argument: $release_type"
 	exit 1
-fi
-
-read -ep "Are you sure you want to cut a $release_type release? [y/N] " continue
-if [[ $continue != "yes" && $continue != "y" && $continue != "Yes" && $continue != "YES" ]]; then
-	exit
 fi
 
 git pull origin master
@@ -46,5 +47,4 @@ git commit -m "build(release): Release $new_version"
 git tag -a $new_version HEAD -m "Release $new_verion"
 git push origin master --follow-tags
 
-# Requires the hub CLI tool to be installed
 hub release create $new_version -m "Release $new_version" -e
