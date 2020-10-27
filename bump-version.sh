35bb1f6afe3aeb4892d9e768f00db1353e8762b3
Sean Brickley <sean@wabr.io>
2020-10-27 15:26:58 -0400

    build: Release v0.5.21
    * build: Release v0.5.21
    
    * fix(release): set correct upstream in bump-version.sh
    
    * chore(build): import latest flux release

--- bump-version.sh
+++ bump-version.sh
@@ -37,15 +37,17 @@ version=v$(cat Cargo.toml | grep -Po -m 1 '\d+\.\d+\.\d+')
 cargo install -q cargo-bump && cargo bump $release_type
 new_version=v$(cat Cargo.toml | grep -Po -m 1 '\d+\.\d+\.\d+')
 
-git checkout -B bump-$new_version
-echo "Checking out branch \`bump-$new_version\`"
+branch_name=bump-$new_version
+
+git checkout -B $branch_name
+echo "Checking out branch \`$branch_name\`"
 
 echo "Incrementing version"
 echo "$version -> $new_version"
 
 git add .
 git commit -m "build: Release $new_version"
-git push origin master
+git push -u origin $branch_name
 
 hub pull-request -o \
 	-m "build: Increment version" \
