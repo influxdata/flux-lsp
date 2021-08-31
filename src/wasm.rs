5bbc3eaec3e2ad025e323b8a1011bc12aaba0505
barweiner <bweiner@influxdata.com>
2021-08-31 12:39:43 -0700

    chore: added test for get_all_docs()

--- src/wasm.rs
+++ src/wasm.rs
@@ -153,12 +153,14 @@ pub fn parse(s: &str) -> JsValue {
 /// Format a JS file.
 #[wasm_bindgen]
 pub fn format_from_js_file(js_file: JsValue) -> String {
-    if let Ok(file) = js_file.into_serde::<File>() {
-        if let Ok(converted) = convert_to_string(&file) {
-            return converted;
-        }
-    }
-    "".to_string()
+    let s = match js_file.into_serde::<File>() {
+        Ok(file) => match convert_to_string(&file) {
+            Ok(converted) => converted,
+            Err(e) => e.to_string(),
+        },
+        Err(e) => e.to_string(),
+    };
+    s
 }
 
 /// Gets json docs for the entire stdlib
@@ -194,6 +196,7 @@ mod tests {
     use flux::semantic::bootstrap::*;
     use flux::semantic::bootstrap::Doc;
 
+    ///Retrieve full PackageDoc documentation for a single flux identifier
     #[wasm_bindgen_test]
     pub fn json_docs_test() {
         let csv_doc = r#"{"path":"csv","name":"csv","headline":"Package csv provides tools for working with data in annotated CSV format.","description":null,"members":{"from":{"kind":"Function","name":"from","headline":"from is a function that retrieves data from a comma separated value (CSV) data source. ","description":"A stream of tables are returned, each unique series contained within its own table. Each record in the table represents a single point in the series. ## Query anotated CSV data from file\n```\nimport \"csv\"\n\ncsv.from(file: \"path/to/data-file.csv\")\n```\n\n## Query raw data from CSV file\n```\nimport \"csv\"\n\ncsv.from(\n  file: \"/path/to/data-file.csv\",\n  mode: \"raw\"\n)\n```\n\n## Query an annotated CSV string\n```\nimport \"csv\"\n\ncsvData = \"\n#datatype,string,long,dateTime:RFC3339,dateTime:RFC3339,dateTime:RFC3339,string,string,double\n#group,false,false,false,false,false,false,false,false\n#default,,,,,,,,\n,result,table,_start,_stop,_time,region,host,_value\n,mean,0,2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:00Z,east,A,15.43\n,mean,0,2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:20Z,east,B,59.25\n,mean,0,2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:40Z,east,C,52.62\n\"\n\ncsv.from(csv: csvData)\n\n```\n\n## Query a raw CSV string\n```\nimport \"csv\"\n\ncsvData = \"\n_start,_stop,_time,region,host,_value\n2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:00Z,east,A,15.43\n2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:20Z,east,B,59.25\n2018-05-08T20:50:00Z,2018-05-08T20:51:00Z,2018-05-08T20:50:40Z,east,C,52.62\n\"\n\ncsv.from(\n  csv: csvData,\n  mode: \"raw\"\n)\n```\n\n","parameters":[{"name":"csv","headline":" is CSV data.","description":"Supports anonotated CSV or raw CSV. Use mode to specify the parsing mode.","required":false},{"name":"file","headline":" is the file path of the CSV file to query.","description":"The path can be absolute or relative. If relative, it is relative to the working directory of the  fluxd  process. The CSV file must exist in the same file system running the  fluxd  process.","required":false},{"name":"mode","headline":" is the CSV parsing mode. Default is annotations.","description":"Available annotation modes: annotations: Use CSV notations to determine column data types. raw: Parse all columns as strings and use the first row as the header row and all subsequent rows as data.","required":false}],"flux_type":"(?csv:string, ?file:string, ?mode:string) => [A] where A: Record","link":"https://docs.influxdata.com/influxdb/cloud/reference/flux/stdlib/csv/from"}},"link":"https://docs.influxdata.com/influxdb/cloud/reference/flux/stdlib/csv"}"#;
@@ -201,76 +204,11 @@ mod tests {
         let got = get_json_documentation("csv");
         assert_eq!(want, got);
     }
-    // #[wasm_bindgen_test]
-    // pub fn parse_test() {
-    //     fn pipe_expression() {
-    //         let mut p = Parser::new(r#"from() |> count()"#);
-    //         let parsed = p.parse_file("".to_string());
-    //         let loc = Locator::new(&p.source[..]);
-    //         assert_eq!(
-    //             parsed,
-    //             File {
-    //                 base: BaseNode {
-    //                     location: loc.get(1, 1, 1, 18),
-    //                     ..BaseNode::default()
-    //                 },
-    //                 name: "".to_string(),
-    //                 metadata: "parser-type=rust".to_string(),
-    //                 package: None,
-    //                 imports: vec![],
-    //                 body: vec![Statement::Expr(Box::new(ExprStmt {
-    //                     base: BaseNode {
-    //                         location: loc.get(1, 1, 1, 18),
-    //                         ..BaseNode::default()
-    //                     },
-    //                     expression: Expression::PipeExpr(Box::new(PipeExpr {
-    //                         base: BaseNode {
-    //                             location: loc.get(1, 1, 1, 18),
-    //                             ..BaseNode::default()
-    //                         },
-    //                         argument: Expression::Call(Box::new(CallExpr {
-    //                             base: BaseNode {
-    //                                 location: loc.get(1, 1, 1, 7),
-    //                                 ..BaseNode::default()
-    //                             },
-    //                             callee: Expression::Identifier(Identifier {
-    //                                 base: BaseNode {
-    //                                     location: loc.get(1, 1, 1, 5),
-    //                                     ..BaseNode::default()
-    //                                 },
-    //                                 name: "from".to_string()
-    //                             }),
-    //                             lparen: vec![],
-    //                             arguments: vec![],
-    //                             rparen: vec![],
-    //                         })),
-    //                         call: CallExpr {
-    //                             base: BaseNode {
-    //                                 location: loc.get(1, 11, 1, 18),
-    //                                 ..BaseNode::default()
-    //                             },
-    //                             callee: Expression::Identifier(Identifier {
-    //                                 base: BaseNode {
-    //                                     location: loc.get(1, 11, 1, 16),
-    //                                     ..BaseNode::default()
-    //                                 },
-    //                                 name: "count".to_string()
-    //                             }),
-    //                             lparen: vec![],
-    //                             arguments: vec![],
-    //                             rparen: vec![],
-    //                         }
-    //                     }))
-    //                 }))],
-    //                 eof: vec![],
-    //             },
-    //         )
-    //     }
-    // }
 
+    ///Retrieve full vector of PackageDoc documentation for entire stdlib
     #[wasm_bindgen_test]
     pub fn all_docs_test() {
-        let docs: Vec<PackageDoc> = serde_json::from_slice(&JsValue::into_serde(&get_all_docs()).unwrap()).unwrap();
+        let docs: Vec<PackageDoc> = serde_json::from_slice(&docs_json().unwrap()).unwrap();
         let mut got: PackageDoc = PackageDoc {
             path: String::new(),
             name: String::new(),
