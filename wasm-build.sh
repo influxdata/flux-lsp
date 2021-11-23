27513dc22b2983ec593def0ed224b7c92bb4b332
Markus Westerlind <mwesterlind@influxdata.com>
2021-11-23 18:02:08 +0100

    chore: Include the Cargo.lock for consistent builds (#340)
    * chore: Include the Cargo.lock for consitent builds
    
    If we do not include it we will build with whatever the newest dependencies are which leads to builds that can't be repeated.
    
    * chore: Ensure we do not alter the lockfile on CI
    
    * chore: Lockdown lspower's version as it does not follow semver

--- wasm-build.sh
+++ wasm-build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-wasm-pack build -t nodejs -d pkg-node --out-name flux-lsp-node --scope influxdata --release
-wasm-pack build -t browser -d pkg-browser --out-name flux-lsp-browser --scope influxdata --release
+wasm-pack build -t nodejs -d pkg-node --out-name flux-lsp-node --scope influxdata --release -- --locked
+wasm-pack build -t browser -d pkg-browser --out-name flux-lsp-browser --scope influxdata --release -- --locked
 
 cat pkg-node/package.json | sed s/@influxdata\\/flux-lsp\"/@influxdata\\/flux-lsp-node\"/g > pkg-node/package-new.json
 mv pkg-node/package-new.json pkg-node/package.json
